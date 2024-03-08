b181279affa34bd3c19484845ca5d2dca2316480
Nour275 <nourh471@gmail.com>
2024-03-08 16:29:45 +0300

    Revert "Introduce syntax error in backup.sh"
    This reverts commit 7e15dd985503e0e5975169e354f60847b5f7a58a.

--- backup.sh
+++ backup.sh
@@ -1,9 +1,3 @@
 echo 'Hello, this is an artbitrary script!
 
 Here's a new line  #with a comment.
-
-# Before
-echo 'This is a valid command'
-
-# After (Introduce syntax error)
-echo 'This is an invalid command
